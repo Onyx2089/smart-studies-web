5c8377b93dcf24fcf8ead9d70aa7a49c5ff4cb6b
onyx <onyx.pixart@gmail>
2022-06-10 01:07:56 +0200

    #5

--- script/async/script.async.Login.php
+++ script/async/script.async.Login.php
@@ -1,3 +1,36 @@
 <?php 
 
-echo "hey super";
+require_once __DIR__ . '/../../model/class/model.class.ApiCall.php';
+
+//echo "script for login ";
+
+if(isset($_GET['email']) && isset($_GET['password']))
+{
+    //echo 'get';
+    $array = ApiCall::get(ApiCall::MODEL_PROFIL, "EMAIL", $_GET['email']);
+
+    //print_r($res);
+    foreach($array as $res)
+    {
+        if($res->PASSWORD == $_GET['password'])
+        {
+            //print_r($res);
+            $stat = $res->STAT;
+            if($stat == 1000)
+            {
+                echo "ADMIN";
+            }
+            elseif($stat == 2000)
+            {
+                echo "STUDENT";
+            }
+
+            session_start();
+
+            $_SESSION['CURSUS'] = $res->CURSUS;
+        }
+        //print_r($res);
+    }
+}
+
+//if()
