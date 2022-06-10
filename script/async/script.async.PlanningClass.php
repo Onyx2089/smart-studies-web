70250f1d9581d2849cbbda99c45b98377816dd68
onyx <onyx.pixart@gmail>
2022-06-10 14:49:09 +0200

    #5

---
+++ script/async/script.async.PlanningClass.php
@@ -0,0 +1,21 @@
+<?php 
+
+require_once __DIR__ . '/../../model/class/model.class.ApiCall.php';
+
+if(isset($_GET['cursus']) && isset($_GET['time']))
+{
+    $array = ApiCall::get(ApiCall::MODEL_CLASS, "CURSUS", $_GET['cursus']);
+    $res = array();
+
+    foreach($array as $class)
+    {
+        if(strstr($class->TIME, $_GET['time']) != false)
+        {
+            $res[] = $class;
+        }
+    }
+
+    print_r(json_encode($res));
+    //if(array_multisort($res, SORT_ASC)){}
+
+}
--- script/async/script.async.PlanningProject.php
+++ script/async/script.async.PlanningProject.php
@@ -4,9 +4,10 @@ require_once __DIR__ . '/../../model/class/model.class.ApiCall.php';
 
 if(isset($_GET['cursus']) && isset($_GET['time']))
 {
-    $array = ApiCall::get(ApiCall::MODEL_CLASS, "CURSUS", $_GET['cursus']);
-    $res = array();
+    $array = ApiCall::get(ApiCall::MODEL_PROJECT, "CURSUS", $_GET['cursus']);
+    //$res = array();
 
+    /*
     foreach($array as $class)
     {
         if(strstr($class->TIME, $_GET['time']) != false)
@@ -14,8 +15,9 @@ if(isset($_GET['cursus']) && isset($_GET['time']))
             $res[] = $class;
         }
     }
+    */
 
-    print_r($res);
+    print_r(json_encode($array));
     //if(array_multisort($res, SORT_ASC)){}
 
 }
