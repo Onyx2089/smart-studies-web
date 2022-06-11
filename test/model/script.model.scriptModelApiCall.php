3f90d79b60df144c1fc4812335a99aff0b5bcc2d
onyx <onyx.pixart@gmail>
2022-06-11 22:55:28 +0200

    #6

--- test/model/script.model.scriptModelApiCall.php
+++ test/model/script.model.scriptModelApiCall.php
@@ -11,14 +11,23 @@ $curl = curl_init();
 
 if($argv[1] == $array[0])
 {
-    $url = "http://localhost:82/git/api_final/?model=profil&field=EMAIL&value=arthur.rubiralta@ecole-89.com";
-    
+    $field = json_encode(array("CURSUS", "TIME"));
+    $op = json_encode(array("EQ", "LIKE"));
+    $value = json_encode(array(3000, "2022-06-11"));
+    $order = json_encode(array("TIME", "OBA"));
+
+    //$url = "http://localhost/git/api_final/?model=class&field=" + $field + "&op=" + $op + "&value=" + $value ;
+    $url = sprintf("http://localhost/git/api_final/?model=class&field=%s&op=%s&value=%s&sort=%s", $field, $op, $value, $order);
+
+    //echo $url . PHP_EOL;
+    //die();
+
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HTTPGET, true);
 }
 elseif($argv[1] == $array[1])
 {
-    $url = "http://localhost:82/git/api_final/?model=profil";
+    $url = "http://localhost/git/api_final/?model=profil";
     
     $data = array('NAME' => 'Issam', 'BIRTH' => '1990-01-06', 'EMAIL' => 'issam@ecole-89.com', 'CURSUS' => 3000, 'STAT' => 1000);
 
@@ -29,7 +38,7 @@ elseif($argv[1] == $array[1])
 }
 elseif($argv[1] == $array[2])
 {
-    $url = "http://localhost:82/git/api_final/?model=project";
+    $url = "http://localhost/git/api_final/?model=project";
     
     //$data = array(/*'ID' => 'NOP', */'NAME' => 'ECO', 'TIME' => '2022-06-06 26:56:00');
     $data = array('ID' => '8', 'NAME' => 'Blockchain', 'DEADLINE' => '2012-06-06 23:56:00');
@@ -49,7 +58,7 @@ elseif($argv[1] == $array[2])
 }
 elseif($argv[1] == $array[3])
 {
-    $url = "http://localhost:82/git/api_final/?model=project&ID=8";
+    $url = "http://localhost/git/api_final/?model=project&ID=8";
     
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
