76e9fe07a3e7fef9dda5dba4d43adf842bda637d
onyx <onyx.pixart@gmail>
2022-06-13 20:23:52 +0200

    #10

--- lib/dashboard/lib.dashboard.FormHtml.php
+++ lib/dashboard/lib.dashboard.FormHtml.php
@@ -1,16 +1,72 @@
 <?php
 
 require_once __DIR__ . '/lib.dashboard.IFormHtml.php';
+require_once __DIR__ . '/../../model/interface/model.interface.IModel.php';
 
-class FormHtml implements IFormHtml
+class FormHtml implements IFormHtml, IModel
 {
     public static function getCreateForm($array)
     {
-        return $array;
+        $html = sprintf(self::SELECT_TYPE, self::BTN, '');
+        
+        $content = self::getFormInput($array);
+
+        $html .= sprintf(self::CONTAINER, $content);
+
+        //return print_r($array);
+
+        return $html;
     }
 
     public static function getModifForm($array)
     {
-        return $array;
+        $html = sprintf(self::SELECT_TYPE, '', self::BTN);
+        $html .= self::CONTAINER;
+
+        return $html;
+    }
+
+    public static function getFormInput($array)
+    {
+        $content = '';
+        foreach($array as $key => $field)
+        {
+            $input = false;
+
+            if($field[0] == self::STR)
+            {
+                $input = $field[1] . " :" . sprintf(self::INPUT, self::INPUT_TEXT, "...");
+                //$input = sprintf("<span>%s</span>", $input);
+            }
+            elseif($field[0] == self::INT)
+            {
+                $input = $field[1] . " :" . sprintf(self::INPUT, self::INPUT_NBR, "...") . "minutes";
+            }
+            elseif($field[0] == self::DATETIME)
+            {
+                $input = $field[1] . " :" . sprintf(self::INPUT, self::INPUT_DATETIME, "...");
+            }
+            elseif($field[0] == self::ARRAY)
+            {
+                $list = sprintf(self::LIST, $key);
+                $options = '';
+
+                foreach($field[2] as $value)
+                {
+                    $options .= sprintf(self::OPTION, $value);
+                }
+
+                $input .= $field[1] . " :" . $list . sprintf(self::DATALIST, $key, $options);
+            }
+            
+            if($input != false)
+            {
+                $content .= sprintf(self::DIV_FIELD, $input);
+            }
+        }
+        
+        $content .= self::VALIDATE;
+
+        return $content;
     }
 }
--- lib/dashboard/lib.dashboard.IFormHtml.php
+++ lib/dashboard/lib.dashboard.IFormHtml.php
@@ -2,5 +2,30 @@
 
 interface IFormHtml
 {
-    
+    const BTN = "btnCM";
+    const SELECT_TYPE = '<div class="f f1"> <div class="f ai-center jc-center %s f1 two-b"> <span class="one-f ft-1_5">Créer</span> </div> <div class="f ai-center jc-center %s f1 two-b"> <span class="one-f ft-1_5">Modifier</span> </div> </div>';
+
+    const CONTAINER = '<div class="f flex-col f7"> %s </div>';
+    const DIV_FIELD = '<div class="one f ai-center jc-center f1 wsn">  <span class="f ai-center jc-center ft-1_8 one-f"> %s </span> </div>';
+    const VALIDATE = '<div class="four f ai-center jc-center f1"> <div class="btn-validate f flex-col ai-center jc-center"> <span class="one-f ft-1">Créer</span> </div> </div>';
+
+
+    const INPUT = '<input type="%s" class="input-t ft-1_5 one-f mrg-left-10 it" placeholder="%s">';
+
+    const INPUT_TEXT = "text";
+    const INPUT_EMAIL = "email";
+    const INPUT_NBR = "number";
+    const INPUT_DATETIME = "datetime-local";
+
+    const LIST = '<input list="%s">';
+    const DATALIST = '<datalist id="%s">%s</datalist>';
+    const OPTION = '<option value="%s">';
+
+    /*
+    const INPUT_TEXT = '<input type="text" class="input-t ft-1_5 one-f mrg-left-10 it" placeholder="%s">';
+    const INPUT_EMAIL = '<input type="email" class="input-t ft-1_5 one-f mrg-left-10 it" placeholder="%s">';
+    const INPUT_NBR = '<input type="number" class="input-t ft-1_5 one-f mrg-left-10 it" placeholder="%s">';
+    const INPUT_DATETIME = '<input type="datetime-local" class="input-t ft-1_5 one-f mrg-left-10 it">';
+    */
+
 }
