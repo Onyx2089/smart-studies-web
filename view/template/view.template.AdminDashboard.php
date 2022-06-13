5e13defdbd8251431fa8d7258d8c16951425c7e3
onyx <onyx.pixart@gmail>
2022-06-13 23:55:15 +0200

    #7

--- view/template/view.template.AdminDashboard.php
+++ view/template/view.template.AdminDashboard.php
@@ -15,45 +15,93 @@
         
         <div class="data f flex-col f5 two-b op-02">
             <?= $content ?>
-        </div>
 
-        <!--<?php print_r($content); ?>-->
-
-        <!--
-        <div class="data f flex-col f5 two-b op-02">
-
+            <!--
             <div class="f f1">
-                <div class="f ai-center jc-center btnCM f1 two-b">
+                <div class="f ai-center jc-center f1 two-b">
                     <span class="one-f ft-1_5">Créer</span>
                 </div>
-                <div class="f ai-center jc-center f1 two-b">
+                <div class="f ai-center jc-center btnCM f1 two-b">
                     <span class="one-f ft-1_5">Modifier</span>
                 </div>
             </div>
-            <div class="f flex-col f7">
-                <div class="one f ai-center jc-center f1">
-                    <span class="f ai-center jc-center ft-1_8 one-f">Nom : inconue</span>
+            <div class="f f7">
+                <div class="f flex-col f1">
+                    <div class="f f1">
+                        <div class="f ai-center jc-end f1">
+                            <span class="one-f ft-1_8">Type</span>
+                        </div>
+                        <div class="f ai-center jc-center f2">
+                            <div class="btn-validate search f flex-col ai-center jc-center three-b">
+                                <span class="one-f ft-1">Inconnue ...</span>
+                            </div>
+                        </div>
+                        <div class="f ai-center f1">
+                            <div class="f ai-center jc-center">
+                                <i class="fa fa-search w3-text-teal" style="font-size:64px"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="f flex-col ai-center f5">
+                        <div class="f flex-col ai-center scrool-search">
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                            <span class="one-f ft-1_5 mrg-top-2">prénom.nom@gmail.com</span>
+                        </div>
+                    </div>
                 </div>
-                <div class="one f ai-center jc-center f1">
-                    <span class="f ai-center jc-center ft-1_8 one-f">Prénom : inconue</span>
-                </div>
-                <div class="one f ai-center jc-center f1">
-                    <span class="f ai-center jc-center ft-1_8 one-f">Date de naissance : jj/mm/aaa</span>
-                </div>
-                <div class="two f ai-center jc-center f1">
-                    <span class="f ai-center jc-center ft-1_8 one-f">Email : nom.prénom@gmail.com</span>
-                </div>
-                <div class="three f ai-center jc-center f1">
-                    <span class="f ai-center jc-center ft-1_8 one-f">Classe : inconnue<span>
-                </div>
-                <div class="four f ai-center jc-center f1">
-                    <div class="btn-validate f flex-col ai-center jc-center">
-                        <span class="one-f ft-1">Créer</span>
+                <div class="f flex-col f1">
+                    <div class="f flex-col f5">
+                        <div class="one f ai-center jc-center f1">
+                            <span class="f ai-center jc-center ft-1_8 one-f">Nom : inconue</span>
+                        </div>
+                        <div class="one f ai-center jc-center f1">
+                            <span class="f ai-center jc-center ft-1_8 one-f">Prénom : inconue</span>
+                        </div>
+                        <div class="one f ai-center jc-center f1">
+                            <span class="f ai-center jc-center ft-1_8 one-f">Date de naissance : jj/mm/aaa</span>
+                        </div>
+                        <div class="two f ai-center jc-center f1">
+                            <span class="f ai-center jc-center ft-1_8 one-f">Email : nom.prénom@gmail.com</span>
+                        </div>
+                    </div>
+                    <div class="f f1">
+                        <div class="f ai-center jc-center f1">
+                            <div class="btn-validate search f flex-col ai-center jc-center three-b">
+                                <span class="one-f ft-1">Modifier</span>
+                            </div>
+                        </div>
+                        <div class="f ai-center jc-center f1">
+                            <div class="f ai-center jc-center">
+                                <i class="fa fa-trash w3-text-red" style="font-size:64px"></i>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             -->
 
+        </div>
+
+        
+            
+
+        <?php LinkClass::getLink($js, LinkClass::TYPE_JS); ?>
 
     </body>
 </html>
--- view/template/view.template.Base.php
+++ view/template/view.template.Base.php
@@ -5,6 +5,8 @@
     <title><?= $title ?></title>
     
     <?php LinkClass::getLink($css, LinkClass::TYPE_CSS) ?>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     
 </head>
