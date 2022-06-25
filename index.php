79d006a7e336ced799fa55d5e224c9f890679341
onyx <onyx.pixart@gmail>
2022-06-26 00:02:08 +0200

    #3

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once __DIR__ . '/config/config.ConfigInterface.php';
+require_once __DIR__ . '/config/config.Iconfig.php';
 require_once __DIR__ . '/lib/link/lib.link.LinkClass.php'; //'/view/link/view.link.LinkClass.php';
-require_once __DIR__ .  '/controller/controller.ControllerLogin.php';
-require_once __DIR__ .  '/controller/controller.ControllerPlanning.php';
+require_once __DIR__ . '/controller/controller.ControllerLogin.php';
+require_once __DIR__ . '/controller/controller.ControllerPlanning.php';
 require_once __DIR__ . '/controller/controller.ControllerAdmin.php';
 require_once __DIR__ . '/controller/controller.ControllerAdminDashBoard.php';
 
