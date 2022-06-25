556fd622db6fb363d5d3f5954dccd5a85e495b9a
onyx <onyx.pixart@gmail>
2022-06-26 00:03:47 +0200

    #9

--- model/class/model.class.IApiCall.php
+++ model/class/model.class.IApiCall.php
@@ -1,8 +1,10 @@
 <?php
 
+require_once __DIR__ . '/../../config/config.IConfig.php';
+
 interface IApiCall
 {
-    const URL_API = "http://localhost/git/api_final/?";
+    const URL_API = IConfig::API; //"http://localhost/git/api_final/?";
 
     const MODEL_CLASS = "class";
     const MODEL_PROFIL = "profil";
