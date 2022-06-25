9e92d4be16b806b486df5d10103a1623f73c1283
onyx <onyx.pixart@gmail>
2022-06-26 00:05:30 +0200

    #5

---
+++ action/action.ActionLogin.php
@@ -0,0 +1,45 @@
+<?php 
+
+require_once __DIR__ . '/../model/class/model.class.ApiCall.php'; 
+
+//echo "script for login ";
+
+if(isset($_GET['email']) && isset($_GET['password']))
+{
+    //echo 'get';
+    $field = array("EMAIL", "PASSWORD");
+    $op = array("EQ", "EQ");
+    $value = array($_GET['email'], $_GET['password']);
+
+    $array = ApiCall::get(ApiCall::MODEL_PROFIL, $field, $op, $value);
+
+    //print_r($array);
+    //die();
+    //print_r($res);
+
+    foreach($array as $res)
+    {
+        //var_export($res->PASSWORD == $_GET['password']);
+        if($res->PASSWORD == $_GET['password'])
+        {
+            //echo 'here';
+            //print_r($res);
+            $stat = $res->STAT;
+            if($stat == 1000)
+            {
+                echo "ADMIN";
+            }
+            elseif($stat == 2000)
+            {
+                echo "STUDENT";
+            }
+
+            session_start();
+
+            $_SESSION['CURSUS'] = $res->CURSUS;
+        }
+        //print_r($res);
+    }
+}
+
+//if()
---
+++ action/action.ActionPlanningClass.php
@@ -0,0 +1,29 @@
+<?php 
+
+require_once __DIR__ . '/../model/class/model.class.ApiCall.php'; 
+
+if(isset($_GET['cursus']) && isset($_GET['time']))
+{
+    $field = array("CURSUS", "TIME");
+    $op = array("EQ", "LIKE");
+    $value = array($_GET['cursus'], $_GET['time']);
+    $order = array("TIME", "OBA");
+
+    $array = ApiCall::get(ApiCall::MODEL_CLASS, $field, $op, $value, $order);
+
+    //echo $array;
+    //print_r($array);
+    
+    print_r(json_encode($array));
+    //print_r($value);
+
+    foreach($array as $class)
+    {
+        $res[] = $class;
+        
+    }
+
+    //print_r(json_encode($res));
+    //if(array_multisort($res, SORT_ASC)){}
+
+}
