cc4204882f65c6145ac4f72f8e94e4f2e0f812cb
onyx <onyx.pixart@gmail>
2022-06-26 00:01:44 +0200

    #1

--- config/config.IConfig.php
+++ config/config.IConfig.php
@@ -1,7 +1,9 @@
 <?php
 
-interface ConfigInterface
+interface IConfig
 {
     const APP_NAME = "Final";//"Unknow";
     const APP_DIR = "/git/web_final/";
+
+    const API = "http://localhost/git/api_final/?";
 }
